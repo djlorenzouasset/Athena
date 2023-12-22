c7de9e9f028e362c74d7c3f43c622d0f8f7b2fc7
ᴅᴊʟᴏʀ3xᴢᴏ <lorenzogame85@gmail.com>
2023-12-23 00:19:02 +0100

    initial common core support

---
+++ Athena/Models/ProfileCommonCore.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+
+namespace Athena.Models;
+
+public class ProfileCommonCore
+{
+    public string _id = Config.config.athenaProfileId;
+    public DateTime created = DateTime.Now;
+    public DateTime updated = DateTime.Now;
+    public int rvn = 100;
+    public int wipeNumber = 1;
+    public string accountId = Config.config.athenaProfileId;
+    public string profileId = "common_core";
+    public string version = Config.config.athenaProfileId;
+    public Items items = new();
+    public Stats stats = new();
+}
+
+public class Items
+{
+    [JsonProperty("Currency:MtxPurchased")]
+    public CurrencyMtxPurchased currencyAttributes = new();
+}
+
+public class Stats
+{
+    public StatsAttributes attributes = new();
+}
+
+public class CurrencyMtxPurchased
+{
+    public CurrencyAttributes attributes = new();
+    public int quantity { get; set; } = 0;
+    public string templateId = "Currency:MtxPurchased";
+}
+
+public class CurrencyAttributes
+{
+    public string platform = "EpicPC";
+}
+
+public class StatsAttributes
+{
+    public string mtx_affiliate = "Neonite";
+    public string current_mtx_platform = "EpicPC";
+    public object mtx_purchase_history = new(); // not used
+}
---
+++ Athena/Services/CommonCoreBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Athena.Models;
+
+namespace Athena.Managers;
+
+public class CommonCoreBuilder
+{
+    private ProfileCommonCore _profile;
+
+    public CommonCoreBuilder()
+    {
+        _profile = new();
+    }
+
+    public string Build() => JsonConvert.SerializeObject(_profile, Formatting.Indented);
+
+    public void SetVbucksAmount(int amount = 0)
+    {
+        _profile.items.currencyAttributes.quantity = amount;
+    }
+
+    // not used atm, here just for a future use (i dont know lol)
+    public void SetCreatorCode(string creatorCode) 
+    {
+        _profile.stats.attributes.mtx_affiliate = creatorCode;
+    }
+}
