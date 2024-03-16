41b1b249a022f6ca6b05615fbf49e383e1f46c2c
ᴅᴊʟᴏʀ3xᴢᴏ <lorenzogame85@gmail.com>
2024-03-16 03:45:10 +0100

    quest model

---
+++ Athena/Models/Quest.cs
@@ -0,0 +1,25 @@
+﻿namespace Athena.Models;
+
+public class ProfileQuest
+{
+    public ProfileQuest(string questId, string rarity)
+    {
+        templateId = $"Quest:{questId.ToLower()}";
+        attributes.quest_rarity = rarity.ToLower();
+    }
+
+    public string templateId { get; set; }
+    public QuestAttributes attributes = new();
+    public int quantity = 1;
+}
+
+public class QuestAttributes
+{
+    public DateTime creation_time = DateTime.Now;
+    public string quest_state = "Active";
+    public DateTime last_state_change_time = DateTime.Now;
+    public int level = -1;
+    public string challenge_bundle_id = Helper.GenerateRandomGuid();
+    public string quest_rarity { get; set; }
+    public int xp_reward_scalar = 1;
+}
