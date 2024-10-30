1d9f749fa89fc8acb35f0b3ae374913f7e3fbfdb
ᴅᴊʟᴏʀ3xᴢᴏ <lorenzogame85@gmail.com>
2024-10-30 13:08:43 +0100

    moved dataminer functions to `dataminer.cs` + added some debug logs

--- Athena/Managers/Athena.cs
+++ Athena/Managers/Athena.cs
@@ -1,6 +1,6 @@
-﻿using RestSharp;
+﻿using System.Runtime.InteropServices;
+using Serilog.Events;
 using Serilog.Sinks.SystemConsole.Themes;
-using Athena.Rest;
 using Athena.Models;
 using Athena.Services;
 
@@ -8,98 +8,35 @@ namespace Athena.Managers;
 
 public static class Athena
 {
-    private static string mapping { get; set; } = string.Empty;
-    private static RestResponse manifest { get; set; } = new();
-
     public static async Task Initialize()
     {
-        Console.Title = "Athena: Starting";
+        Console.Title = "Athena: Starting"; // do we really need this??
 
-        // configure logger for the application
-        Log.Logger = new LoggerConfiguration()
-            .WriteTo.File(Path.Combine(DirectoryManager.Logs, $"Athena-Log-{DateTime.Now:dd-MM-yyyy}.txt"))
-            .WriteTo.Console(theme: AnsiConsoleTheme.Literate)
+        Log.Logger = new LoggerConfiguration().WriteTo
+            .Console(LogEventLevel.Information, theme: AnsiConsoleTheme.Literate).WriteTo
+            .File(Path.Combine(DirectoryManager.Logs, $"Athena-Log-{DateTime.Now:dd-MM-yyyy}.log"))
             .CreateLogger();
 
-        DirectoryManager.CreateFolders(); // this function creates the needed folders
-        Config.LoadSettings(); // create the settings file or load the saved one
-        Console.Clear(); // clear the console after input
+        /* informations needed for support */
+        Log.Information("      --------------------------------------      ");
+        Log.Information("Log file opened: {date}", DateTime.Now);
+        Log.Information("Athena version is: {ver}", Globals.VERSION);
+        Log.Information(".NET version is: {RuntimeVer}", RuntimeInformation.FrameworkDescription);
 
-        // discord rich presence 
-        DiscordRichPresence.Initialize();
+#if DEBUG
+        Log.Information("bIsDebug: {bDebug}", true); // funny
+#else
+        Log.Information("bIsDebug: {bDebug}", false);
+#endif
 
-        await Dataminer.InitializeOodle();
-        await CheckAuth();
-        await TryGetAesKeys();
-        if (!await TryGetMappings()) return;
-        if (!await TryGetManifest()) return;
-        await InitializeDataminer();
-    }
+        Console.Clear(); // clear console after utils logs
+        DirectoryManager.CreateFolders();
+        Config.LoadSettings();
+        // clear the console for a better look in case the user inserted paths 
+        Console.Clear();
 
-    private static async Task TryGetAesKeys()
-    {
-        var aesKeys = await Endpoints.FNCentral.GetAesKeysAsync();
-        if (aesKeys is null)
-        {
-            Log.Warning("AesKeys response was unsuccessful, the program may not work as expected.");
-        }
-    }
-
-    private static async Task<bool> TryGetMappings()
-    {
-        var mappings = await Endpoints.FNCentral.GetMappingsAsync();
-        if (mappings is null || mappings.Length == 0)
-        {
-            if (!DirectoryManager.GetSavedMappings(out string mappingPath))
-            {
-                Log.Error("Mappings response was invalid and no mappings are saved in .mappings directory.");
-                return false;
-            }
-
-            mapping = Path.Combine(DirectoryManager.MappingsDir, mappingPath);
-            Log.Information("Mappings loaded from {path}", mapping);
-        }
-        else
-        {
-            var mappingFile = mappings.First();
-            mapping = Path.Combine(DirectoryManager.MappingsDir, mappingFile.Filename);
-            if (!File.Exists(mapping))
-            {
-                await Endpoints.FNCentral.DownloadMappingsAsync(mappingFile.Url, mappingFile.Filename);
-                Log.Information("Mappings downloaded in {path}", mapping);
-            }
-        }
-
-        return true;
-    }
-
-    private static async Task<bool> TryGetManifest()
-    {
-        RestResponse? _manifest = await Endpoints.Epic.GetManifestAsync();
-        if (_manifest is null || string.IsNullOrEmpty(_manifest.Content))
-        {
-            Log.Error("Invalid response from Fortnite Manifest.");
-            return false;
-        }
-
-        manifest = _manifest;
-        return true;
-    }
-
-    private static async Task CheckAuth()
-    {
-        if (!await Endpoints.Epic.IsAuthValid())
-        {
-            var auth = await Endpoints.Epic.CreateAuthAsync();
-            Config.config.accessToken = auth.access_token;
-            Config.Save();
-        }
-    }
-
-    private static async Task InitializeDataminer()
-    {
-        var dataminer = new Dataminer(mapping);
-        await dataminer.LoadAllPaksAsync(manifest);
-        await dataminer.ShowMenu();
+        DiscordRichPresence.Initialize(); // this will be optional in v2 :/
+        await Dataminer.Instance.Initialize();
+        await Dataminer.Instance.ShowMenu();
     }
 }
