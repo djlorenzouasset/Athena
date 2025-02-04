d19922fa70cdea43264636c0a19cb5a6bda97197
ᴅᴊʟᴏʀ3xᴢᴏ <89230676+djlorenzouasset@users.noreply.github.com>
2025-02-04 04:18:49 +0100

    enums

---
+++ Athena/Enums.cs
@@ -0,0 +1,29 @@
+﻿namespace Athena;
+
+public enum EModelType : int
+{
+    ProfileAthena = 0,
+    ItemShopCatalog = 1
+}
+
+public enum EGenerationType : int
+{
+    AllCosmetics = 0, // >> add all cosmetics
+    NewCosmetics = 1, // >> add only new cosmetics filtered by backup
+    NewCosmeticsAndArchives = 2, // >> add only new cosmetics + encrypted archives
+
+    SingleArchive = 3, // >> add a single archive
+    MultipleArchives = 4, // >> add multiple archives by name or guid
+    WaitForArchivesUpdate = 5, // >> watch for new archives to become availables
+
+    SelectedCosmeticsOnly = 6 // >> add only selected cosmetics by ID/DAv2
+}
+
+public enum EBackupVersion : byte
+{
+    BeforeVersionWasAdded = 0,
+    Initial,
+
+    LatestPlusOne,
+    Latest = LatestPlusOne - 1
+}
