d9ec3568d0060ef4fb5a8b6da4ce9c39c5343824
ᴅᴊʟᴏʀ3xᴢᴏ <89230676+djlorenzouasset@users.noreply.github.com>
2025-02-05 02:56:59 +0100

    CUE4Parse extensions

---
+++ Athena/Extensions/CUE4ParseExtensions.cs
@@ -0,0 +1,9 @@
+﻿using CUE4Parse.UE4.VirtualFileSystem;
+
+namespace Athena.Extensions;
+
+public static class CUE4ParseExtensions
+{
+    public static string CreateObjectPath(this VfsEntry _entry)
+        => $"{_entry.PathWithoutExtension}.{_entry.NameWithoutExtension}";
+}
