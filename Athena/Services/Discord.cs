bcdbe7ea85da16299fa46bd58a52d1dea526f712
ᴅᴊʟᴏʀ3xᴢᴏ <89230676+djlorenzouasset@users.noreply.github.com>
2025-02-05 02:59:18 +0100

    discord client

---
+++ Athena/Services/Discord.cs
@@ -0,0 +1,33 @@
+﻿using DiscordRPC;
+
+namespace Athena.Services;
+
+public static class DiscordRichPresence
+{
+    private const string APP_ID = "1142239120471634042";
+
+    private static DiscordRpcClient _client = null!; 
+    private static readonly RichPresence _defaultPresence = new()
+    {
+        Timestamps = new() { Start = DateTime.UtcNow },
+        Assets = new() { LargeImageKey = "logo", LargeImageText = $"Athena v{Globals.Version}" },
+        Buttons = [
+            new() { Label = "Download", Url = Globals.GITHUB_URL },
+            new() { Label = "Join Athena", Url = Globals.DISCORD_URL }
+        ]
+    };
+
+    public static void Initialize()
+    {
+        _client = new DiscordRpcClient(APP_ID);
+        _client.OnReady += (_, args) => Log.Information("Discord Presence started for {username}", args.User.Username);
+        _client.OnError += (_, args) => throw new Exception($"Error while starting Discord RPC: {args.Message}");
+        _client.Initialize();
+        _client.SetPresence(_defaultPresence);
+    }
+
+    public static void Update(string text)
+    {
+        _client?.UpdateState(text);
+    }
+}
