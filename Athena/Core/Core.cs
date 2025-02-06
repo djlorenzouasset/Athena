48520deb5961dc9c9e3bff304007029fb293a72c
ᴅᴊʟᴏʀ3xᴢᴏ <89230676+djlorenzouasset@users.noreply.github.com>
2025-02-06 04:10:02 +0100

    core + autoupdater

--- Athena/Core/Core.cs
+++ Athena/Core/Core.cs
@@ -4,6 +4,8 @@ using Serilog.Sinks.SystemConsole.Themes;
 
 using Athena.Utils;
 using Athena.Services;
+using Athena.Models.App;
+using Athena.Rest;
 
 namespace Athena.Core;
 
@@ -19,23 +21,40 @@ public class AthenaCore
             .File($"Athena-Log-{DateTime.Now:dd-MM-yyyy}.log")
             .CreateLogger();
 
+        #region DEBUG LOGS
         Log.ForContext("NoConsole", true).Information("      --------------------------------------      ");
         Log.ForContext("NoConsole", true).Information("Log file opened: {date}", DateTime.Now);
-        Log.ForContext("NoConsole", true).Information("Athena version is: {ver}", Globals.Version);
-        Log.ForContext("NoConsole", true).Information("Current folder is {path}", Environment.CurrentDirectory);
-        Log.ForContext("NoConsole", true).Information(".NET version is: {runtimeVer}", RuntimeInformation.FrameworkDescription);
+        Log.ForContext("NoConsole", true).Information("Athena version: {ver}", Globals.Version);
+        Log.ForContext("NoConsole", true).Information("Current folder: {path}", Environment.CurrentDirectory);
+        Log.ForContext("NoConsole", true).Information("NET version: {runtimeVer}", RuntimeInformation.FrameworkDescription);
 #if DEBUG
         Log.ForContext("NoConsole", true).Information("bIsDebug: {bDebug}", true); // funny
 #else
         Log.ForContext("NoConsole", true).Information("bIsDebug: {bDebug}", false);
 #endif
+        #endregion
 
         if (!Directories.IsCurrentDirectoryValid())
         {
-            Log.Error("You can't use Athena in this directory. Please move it in another folder and try again.");
+            Log.Error("You can't run Athena in this directory! Please move it into another folder and try again.");
+            Message.Show("Athena", "You can't run Athena in this directory! Please move it into another folder and try again.", 
+                Message.MB_ICONERROR | Message.MB_OK
+            );
+
             FUtils.ExitThread(1);
         }
 
         Directories.CreateDefaultFolders();
+#if !DEBUG
+        await Updater.Instance.CheckForUpdate();
+#endif
+
+        UserSettings.LoadSettings();
+        Console.Clear(); // clear the console after settings creation/load
+
+        if (UserSettings.Current.bUseDiscordRPC)
+        {
+            DiscordRichPresence.Initialize();
+        }
     }
 }
--- Athena/Services/Updater.cs
+++ Athena/Services/Updater.cs
@@ -1,19 +1,89 @@
-﻿using Athena.Models.App;
+﻿using System.Diagnostics;
+using Athena.Utils;
+using Athena.Models.API.Athena;
 
 namespace Athena.Services;
 
 public class Updater
 {
-    public async Task<bool> CheckForUpdate()
+    public static Updater Instance = new();
+
+    private AthenaRelease Release = null!;
+    private readonly string _currentInstance = Path.Combine(Environment.CurrentDirectory, "Athena.exe");
+    private readonly string _tempReleaseFile = Path.Combine(Directories.Data.FullName, "Athena.exe");
+    private readonly string _updaterFile = Path.Combine(Directories.Data.FullName, "AthenaUpdater.exe");
+    private readonly string _updaterUrl = "https://github.com/djlorenzouasset/Athena/raw/refs/heads/main/Athena.Updater/AthenaUpdater.exe";
+
+    public async Task CheckForUpdate()
     {
-        return false;
+        var release = await APIEndpoints.Athena.GetReleaseInfoAsync();
+        if (release is null)
+        {
+            Log.Warning("Failed to request Athena release informations.");
+            return;
+        }
+
+        Release = release;
+        if (Release.Version > Globals.Version)
+        {
+            var bUpdate = Message.Show("Update available!", $"Athena {Release.Version.DisplayName} is now available. Do you want to install it?", 
+                Message.MB_YESNO | Message.MB_DEFBUTTON1 | Message.MB_ICONINFORMATION
+            );
+
+            if (bUpdate == Message.BT_YES && await DownloadUpdate())
+            {
+                RunUpdater();
+            }
+        }
     }
 
-    public async Task DownloadUpdate()
+    private async Task<bool> DownloadUpdate()
     {
+        if (!File.Exists(_updaterFile))
+        {
+            Log.Information("Athena-Updater is not installed! Installing it..");
+            if (!await APIEndpoints.DownloadFileAsync(_updaterUrl, _updaterFile))
+            {
+                Log.Fatal("Failed to download Athena-Updater. Please contact the staff or download the update manually from github.");
+                return false;
+            }
+        }
+
+        Log.Information("Downloading Athena {ver} [Size: {size}, Release Date: {date}]..", 
+            Release.Version.DisplayName, Release.UpdateSize, Release.ReleaseDate);
+
+        if (!await APIEndpoints.DownloadFileAsync(Release.DownloadUrl, _tempReleaseFile))
+        {
+            Log.Fatal("Failed to update Athena. Please contact the staff or download the update manually from github.");
+            return false;
+        }
+
+        Log.Information("Athena {ver} has been downloaded!", Release.Version);
+        return true;
     }
 
-    public async Task RunUpdater(AthenaVersion newVersion)
+    private void RunUpdater()
     {
+        Log.Information("Starting Athena updater.");
+
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = _updaterFile,
+            Arguments = $"\"{_currentInstance}\" \"{_tempReleaseFile}\" \"{Release.Version}\"",
+            UseShellExecute = true,
+            CreateNoWindow = true,
+            WindowStyle = ProcessWindowStyle.Hidden
+        };
+
+        try
+        {
+            Process.Start(startInfo);
+        }
+        catch (Exception e)
+        {
+            Log.Fatal("Failed to update Athena: {exc}", e.Message);
+        }
+
+        FUtils.ExitThread();
     }
 }
