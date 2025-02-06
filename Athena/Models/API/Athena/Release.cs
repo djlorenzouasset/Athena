9de05f4fd2770f33b771d5d6ad0bb5d90eff3b09
ᴅᴊʟᴏʀ3xᴢᴏ <89230676+djlorenzouasset@users.noreply.github.com>
2025-02-06 04:10:35 +0100

    update size and use JsonConverter in Version

--- Athena/Models/API/Athena/Release.cs
+++ Athena/Models/API/Athena/Release.cs
@@ -1,11 +1,15 @@
-﻿using Athena.Models.App;
+﻿using Newtonsoft.Json;
+using Athena.Models.App;
 
 namespace Athena.Models.API.Athena;
 
 public class AthenaRelease
 {
+    [JsonConverter(typeof(AthenaVersionConverter))]
     public AthenaVersion Version;
+
     public string DownloadUrl;
+    public double UpdateSize;
     public string Changelog;
     public DateTime ReleaseDate;
 }
