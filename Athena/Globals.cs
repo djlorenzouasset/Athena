ec1480cf976455dcdcc77e850b8e99782f92ae2f
ᴅᴊʟᴏʀ3xᴢᴏ <89230676+djlorenzouasset@users.noreply.github.com>
2025-03-21 03:50:47 +0100

    forgot about athena v2 for a while

--- Athena/Globals.cs
+++ Athena/Globals.cs
@@ -9,11 +9,12 @@ public static class Globals
 {
     public static readonly AthenaVersion Version = new(2, 0, 0);
 
-    public const string DISCORD_URL = "https://discord.gg/nJBj9NjUS4";
     public const string GITHUB_URL = "https://github.com/djlorenzouasset/Athena/releases/latest";
+    public const string DISCORD_URL = "https://discord.gg/nJBj9NjUS4";
     public const string DONATIONS_URL = "https://ko-fi.com/djlorenzouasset";
 
     public static readonly FGuid ZERO_GUID = new();
+
     public static readonly JsonSerializerSettings JsonSettings = new()
     {
         ContractResolver = new DefaultContractResolver
