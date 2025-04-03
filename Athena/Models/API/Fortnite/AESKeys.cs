9a42e03a7e0dbceedefe09474e1a602086b5d2e3
ᴅᴊʟᴏʀ3xᴢᴏ <89230676+djlorenzouasset@users.noreply.github.com>
2025-04-04 01:04:46 +0200

    add more infos to AESKeys response

--- Athena/Models/API/Fortnite/AESKeys.cs
+++ Athena/Models/API/Fortnite/AESKeys.cs
@@ -11,4 +11,11 @@ public class DynamicKey
     public string Name;
     public string Key;
     public string Guid;
+    public int FileCount;
+    public Size Size;
+}
+
+public class Size
+{
+    public string Formatted;
 }
