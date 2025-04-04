caacae1c874654d4c9ba2ed27803e8e672674571
ᴅᴊʟᴏʀ3xᴢᴏ <89230676+djlorenzouasset@users.noreply.github.com>
2025-04-04 20:12:54 +0200

    GuidsList property for better fetch

--- Athena/Models/API/Fortnite/AESKeys.cs
+++ Athena/Models/API/Fortnite/AESKeys.cs
@@ -1,9 +1,13 @@
-﻿namespace Athena.Models.API.Fortnite;
+﻿using CUE4Parse.UE4.Objects.Core.Misc;
+
+namespace Athena.Models.API.Fortnite;
 
 public class AESKeys
 {
     public string MainKey;
     public List<DynamicKey> DynamicKeys;
+
+    public List<FGuid> GuidsList => [..DynamicKeys.Select(x => new FGuid(x.Guid))];
 }
 
 public class DynamicKey
