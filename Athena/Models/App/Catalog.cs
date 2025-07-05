5c713bac34b3170b16cd487a5b23ec2dbbc0d4e6
ᴅᴊʟᴏʀ3xᴢᴏ <89230676+djlorenzouasset@users.noreply.github.com>
2025-07-05 19:51:26 +0200

    output models

---
+++ Athena/Models/App/Catalog.cs
@@ -0,0 +1,171 @@
+﻿namespace Athena.Models.App;
+
+public class ShopModel
+{
+    public int RefreshIntervalHrs { get; set; } = 1;
+    public int DailyPurchaseHrs { get; set; } = 24;
+    public DateTime Expiration { get; set; } = new(9999, 12, 31, 23, 59, 59);
+    public List<Storefront> Storefronts { get; set; } = [];
+}
+
+public class Storefront
+{
+    public string Name { get; set; }
+    public List<ICatalogEntry> CatalogEntries { get; set; } = [];
+}
+
+public interface ICatalogEntry
+{
+    string DevName { get; set; }
+    string OfferId { get; set; }
+    List<object> FulfillmentIds { get; set; }
+    int DailyLimit { get; set; }
+    int WeeklyLimit { get; set; }
+    int MonthlyLimit { get; set; }
+    List<string> Categories { get; set; }
+    List<Price> Prices { get; set; }
+    Meta Meta { get; set; }
+    string MatchFilter { get; set; }
+    double FilterWeight { get; set; }
+    List<string> AppStoreId { get; set; }
+    string OfferType { get; set; }
+    GiftInfo GiftInfo { get; set; }
+    bool Refundable { get; set; }
+    List<MetaInfo> MetaInfo { get; set; }
+    string DisplayAssetPath { get; set; }
+    List<ItemGrant> ItemGrants { get; set; }
+    List<object> AdditionalGrants { get; set; }
+    int SortPriority { get; set; }
+    int CatalogGroupPriority { get; set; }
+}
+
+public class BundleCatalogEntry : ICatalogEntry
+{
+    public string DevName { get; set; } = "[VIRTUAL]1 x Funny Thing for -9 MtxCurrency";
+    public string OfferId { get; set; } = string.Empty;
+    public List<object> FulfillmentIds { get; set; } = [];
+    public int DailyLimit { get; set; } = -1;
+    public int WeeklyLimit { get; set; } = -1;
+    public int MonthlyLimit { get; set; } = -1;
+    public List<string> Categories { get; set; } = [];
+    public List<Price> Prices { get; set; } = [];
+    public DynamicBundleInfo DynamicBundleInfo { get; set; } = new();
+    public Meta Meta { get; set; } = new();
+    public string MatchFilter { get; set; } = string.Empty;
+    public double FilterWeight { get; set; } = 0.0;
+    public List<string> AppStoreId { get; set; } = [];
+    public List<object> Requirements { get; set; } = [];
+    public string OfferType { get; set; } = "DynamicBundle";
+    public GiftInfo GiftInfo { get; set; } = new();
+    public bool Refundable { get; set; } = true;
+    public List<MetaInfo> MetaInfo { get; set; } = [];
+    public string DisplayAssetPath { get; set; } = string.Empty;
+    public List<ItemGrant> ItemGrants { get; set; } = [new() { TemplateId = "AthenaCharacter:CID_349_Athena_Commando_M_Banana" }];
+    public List<object> AdditionalGrants { get; set; } = [];
+    public int SortPriority { get; set; } = -1;
+    public int CatalogGroupPriority { get; set; } = 0;
+}
+
+public class CosmeticCatalogEntry : ICatalogEntry
+{
+    public string DevName { get; set; } = "[VIRTUAL]1 x Funny Thing for -9 MtxCurrency";
+    public string OfferId { get; set; } = string.Empty;
+    public List<object> FulfillmentIds { get; set; } = [];
+    public int DailyLimit { get; set; } = -1;
+    public int WeeklyLimit { get; set; } = -1;
+    public int MonthlyLimit { get; set; } = -1;
+    public List<string> Categories { get; set; } = [];
+    public List<Price> Prices { get; set; } = [new()];
+    public Meta Meta { get; set; } = new();
+    public string MatchFilter { get; set; } = string.Empty;
+    public double FilterWeight { get; set; } = 0.0;
+    public List<string> AppStoreId { get; set; } = [];
+    public List<Requirement> Requirements { get; set; } = [];
+    public string OfferType { get; set; } = "StaticPrice";
+    public GiftInfo GiftInfo { get; set; } = new();
+    public bool Refundable { get; set; } = true;
+    public List<MetaInfo> MetaInfo { get; set; } = [];
+    public string DisplayAssetPath { get; set; } = string.Empty;
+    public List<ItemGrant> ItemGrants { get; set; } = [];
+    public List<object> AdditionalGrants { get; set; } = [];
+    public int SortPriority { get; set; } = -2;
+    public int CatalogGroupPriority { get; set; } = 0;
+}
+
+public class Price
+{
+    public string CurrencyType = "MtxCurrency";
+    public string CurrencySubType = string.Empty;
+    public int RegularPrice = UserSettings.Current.Catalog.ItemPrice;
+    public int DynamicRegularPrice = UserSettings.Current.Catalog.ItemPrice;
+    public int FinalPrice = UserSettings.Current.Catalog.ItemPrice;
+    public DateTime SaleExpiration = new(9999, 12, 31, 23, 59, 59);
+    public int BasePrice = UserSettings.Current.Catalog.ItemPrice;
+}
+
+public class DynamicBundleInfo
+{
+    public int DiscountedBasePrice = UserSettings.Current.Catalog.BundlePrice;
+    public int RegularBasePrice = UserSettings.Current.Catalog.BundlePrice;
+    public int FloorPrice = UserSettings.Current.Catalog.BundlePrice;
+    public string CurrencyType = "MtxCurrency";
+    public string CurrencySubType = string.Empty;
+    public string DisplayType  = "AmountOff";
+    public List<BundleRequirement> BundleItems = [new()];
+}
+
+public class Meta
+{
+    public string NewDisplayAssetPath { get; set; }
+    public string SectionId { get; set; }
+    public string LayoutId { get; set; }
+    public string TileSize { get; set; }
+
+    public string AnalyticOfferGroupId = "ISTHISNEEDED??";
+    public string FirstSeen = "31/12/9999";
+    public string Color1 = "#424242";
+    public string Color2 = "#212121";
+    public string Color3 = "#121212";
+}
+
+public class BundleRequirement
+{
+    public bool bCanOwnMultiple = true;
+    public int RegularPrice = UserSettings.Current.Catalog.ItemPrice;
+    public int DiscountedPrice = UserSettings.Current.Catalog.ItemPrice;
+    public int AlreadyOwnedPriceReduction = UserSettings.Current.Catalog.ItemPrice;
+    public Item Item = new() { TemplateId = "AthenaCharacter:CID_349_Athena_Commando_M_Banana", Quantity = 1 };
+}
+
+public class Requirement
+{
+    public string RequirementType = "DenyOnItemOwnership";
+    public string RequiredId { get; set; }
+    public int MinQuantity = 1;
+}
+
+public class GiftInfo
+{
+    public bool bIsEnabled = true;
+    public string ForcedGiftBoxTemplateId = string.Empty;
+    public List<object> PurchaseRequirements = [];
+    public List<object> GiftRecordIds = [];
+}
+
+public class MetaInfo
+{
+    public string Key { get; set; }
+    public string Value { get; set; }
+}
+
+public class ItemGrant
+{
+    public string TemplateId { get; set; }
+    public int Quantity = 10;
+}
+
+public class Item
+{
+    public string TemplateId { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ Athena/Models/App/Profile.cs
@@ -0,0 +1,214 @@
+﻿using Athena.Models.App;
+
+public class Cosmetic
+{
+    public string TemplateId { get; set; }
+    public Attributes Attributes = new();
+    public int Quantity = 1;
+
+    public Cosmetic(string cosmeticId, string backendType, Dictionary<string, List<string>>? variants = null)
+    {
+        TemplateId = $"{backendType}:{cosmeticId}";
+        Attributes.Favorite = false;
+        Attributes.Archived = false;
+
+        if (variants != null && variants.Count > 0)
+        {
+            foreach (var (channel, ownedParts) in variants)
+            {
+                Attributes.Variants.Add(new()
+                {
+                    Channel = channel,
+                    Active = ownedParts.Count > 0 ? ownedParts[0] : "", // scuffed thing for FortCosmeticRichColorVariant
+                    Owned = ownedParts
+                });
+            }
+        }
+    }
+}
+
+public class Attributes
+{
+    // public DateTime CreationTime = DateTime.UtcNow; this causes no variants on >18.00 (???)
+    public int MaxLevelBonus = 0;
+    public int Level = 1;
+    public bool ItemSeen = true;
+    public int RndSelCnt = 0;
+    public int Xp = 0;
+    public List<ProfileAthena.Variant> Variants = [];
+    public bool Favorite = false;
+    public bool Archived = false;
+}
+
+public class ProfileAthena
+{
+    public string Id = "aa479043-3602-40be-959b-853003ea8fce";
+    public DateTime Created = DateTime.UtcNow;
+    public DateTime Updated = DateTime.UtcNow;
+    public int Rvn = 100;
+    public int WipeNumber = 1;
+    public string AccountId = UserSettings.Current.Profiles.ProfileId;
+    public string ProfileId = "athena";
+    public string Version = "";
+    public ProfileItems Items = new();
+    public ProfileStats Stats = new();
+    public int CommandRevision = 100;
+
+    public class ProfileItems
+    {
+        public Loadout SandboxLoadout = new();
+    }
+
+    public class Loadout
+    {
+        public string TemplateId = "CosmeticLocker:cosmeticlocker_athena";
+        public Attributes Attributes = new();
+        public int Quantity = 1;
+    }
+
+    public class Attributes
+    {
+        public LockerSlotsData LockerSlotsData = new();
+        public int UseCount = 1;
+        public string BannerIconTemplate = "BRS11_Prestige5";
+        public string LockerName = UserSettings.Current.Profiles.ProfileId;
+        public string BannerColorTemplate = "DefaultColor40";
+        public bool ItemSeen = false;
+        public bool Favorite = false;
+    }
+
+    public class LockerSlotsData
+    {
+        public Slots Slots = new();
+    }
+
+    public class Slots
+    {
+        public Pickaxe Pickaxe = new();
+        public Dance Dance = new();
+        public Glider Glider = new();
+        public Character Character = new();
+        public Backpack Backpack = new();
+        public ItemWrap ItemWrap = new();
+        public LoadingScreen LoadingScreen = new();
+        public MusicPack MusicPack = new();
+        public SkydiveContrail SkyDiveContrail = new();
+    }
+
+    public class Pickaxe
+    {
+        public List<string> Items = [""];
+        public List<ActiveVariant> ActiveVariants = [];
+    }
+
+    public class Dance
+    {
+        public List<string> Items = [""];
+    }
+
+    public class Glider
+    {
+        public List<string> Items = [""];
+    }
+
+    public class Character
+    {
+        public List<string> Items = ["AthenaCharacter:CID_001_Athena_Commando_F_Default"];
+        public List<ActiveVariant> ActiveVariants = [];
+    }
+
+    public class Backpack
+    {
+        public List<string> Items = [""];
+        public List<ActiveVariant> ActiveVariants = [];
+    }
+
+    public class ItemWrap
+    {
+        public List<string> Items = [""];
+        public List<ActiveVariant> ActiveVariants = [];
+    }
+
+    public class LoadingScreen
+    {
+        public List<string> Items = [""];
+        public List<ActiveVariant> ActiveVariants = [];
+    }
+
+    public class MusicPack
+    {
+        public List<string> Items = [""];
+        public List<ActiveVariant> ActiveVariants = [];
+    }
+
+    public class SkydiveContrail
+    {
+        public List<string> Items = [""];
+        public List<ActiveVariant> ActiveVariants = [];
+    }   
+
+    public class ActiveVariant
+    {
+        public List<Variant> Variants = [];
+    }
+
+    public class Variant
+    {
+        public string Channel { get; set; }
+        public string Active { get; set; }
+        public List<string> Owned { get; set; }
+    }
+
+    public class ProfileStats
+    {
+        public StatAttributes Attributes = new();
+    }
+
+    public class StatAttributes
+    {
+        public int SeasonMatchBoost = 999999;
+        public List<string> Loadouts = ["sandbox_loadout"];
+        public int RestedXpOverflow = 0;
+        public bool MfaRewardClaimed = true;
+        public QuestManager QuestManager = new();
+        public int BookLevel = -1;
+        public int SeasonNum = 999999;
+        public int SeasonUpdate = -1;
+        public int BookXp = -1;
+        public List<object> Permissions = [];
+        public bool BookPurchased = true;
+        public int LifetimeWins = -1;
+        public string PartyAssistQuest = string.Empty;
+        public List<object> PurchasedBattlePassTierOffers = [];
+        public float RestedXpExchange = -1f;
+        public int Level = UserSettings.Current.Profiles.BattlepassLevel;
+        public long XpOverflow = -1;
+        public int RestedXp = -1;
+        public float RestedXpMult = -1f;
+        public int AccountLevel = UserSettings.Current.Profiles.BattlepassLevel;
+        public CompetitiveIdentity CompetitiveIdentity = new();
+        public int InventoryLimitBonus = 0;
+        public string LastAppliedLoadout = "sandbox_loadout";
+        public DailyRewards DailyRewards = new();
+        public int Xp = 10;
+        public int SeasonFriendMatchBoost = 1;
+        public int ActiveLoadoutIndex = 1;
+        public List<PastSeasons> PastSeasons = [];
+    }
+
+    public class QuestManager
+    {
+    }
+
+    public class CompetitiveIdentity
+    {
+    }
+
+    public class DailyRewards
+    {
+    }
+
+    public class PastSeasons
+    {
+    }
+}
