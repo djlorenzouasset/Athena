025f2657a6649bec1b9847b3e8732dc68869138e
ᴅᴊʟᴏʀ3xᴢᴏ <89230676+djlorenzouasset@users.noreply.github.com>
2025-12-03 03:16:25 +0100

    ready for final release

--- Athena/Models/API/AthenaAPI.cs
+++ Athena/Models/API/AthenaAPI.cs
@@ -6,11 +6,7 @@ namespace Athena.Models.API;
 
 public class AthenaEndpoints(RestClient client) : AthenaRestClient(client)
 {
-#if DEBUG // temp
-    protected override string BaseURL => "http://prod.athena.dev:8000";
-#else
     protected override string BaseURL => "https://athena-service-prod.djlorexzo.dev";
-#endif
 
     private const string NEWS_ENDPOINT = "api/v1/news";
     private const string RELEASE_ENDPOINT = "api/v1/version";
