71f0cb14da6dcac3bc43ce84319b0cc283b7c75a
Layla <94177119+LaylaLeaks@users.noreply.github.com>
2025-12-12 04:55:06 +0100

    missing spray definition

--- Athena/Services/AssetsService.cs
+++ Athena/Services/AssetsService.cs
@@ -64,6 +64,7 @@ public class AssetsService
             ClassNames =
             [
                 "AthenaDanceItemDefinition",
+                "AthenaSprayItemDefinition",
                 "AthenaToyItemDefinition",
                 "AthenaEmojiItemDefinition"
             ],
